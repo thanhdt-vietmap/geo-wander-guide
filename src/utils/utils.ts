1eed32c512df972ebac07f478f29fb7e33f62a54
thanhdt-vietmap <thanhdt@vietmap.vn>
2025-05-24 17:13:42 +0700

    feat: update more tile

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -73,6 +73,31 @@ class MapUtils {
     };
   };
 
+  getVietMapRasterTileLayer = () => {
+    return {
+      version: 8,
+      sources: {
+        raster_vm: {
+          type: "raster",
+          tiles: [
+            `https://maps.vietmap.vn/api/tm/{z}/{x}/{y}@2x.png?apikey=95f852d9f8c38e08ceacfd456b59059d0618254a50d3854c`,
+          ],
+          tileSize: 256,
+          attribution: "Vietmap@copyright",
+        },
+      },
+      layers: [
+        {
+          id: "layer_raster_vm",
+          type: "raster",
+          source: "raster_vm",
+          minzoom: 0,
+          maxzoom: 20,
+        },
+      ],
+    };
+  };
+
   getVietMapHybridRasterTileLayer = () => {
     return {
       version: 8,
@@ -135,6 +160,13 @@ class MapUtils {
 
   getVietMapVectorTile = () => {
     return "https://maps.vietmap.vn/mt/tm/style.json?apikey=95f852d9f8c38e08ceacfd456b59059d0618254a50d3854c";
+  };
+  getVietMapVectorDarkTile = () => {
+    return "https://maps.vietmap.vn/mt/dm/style.json?apikey=95f852d9f8c38e08ceacfd456b59059d0618254a50d3854c";
+  };
+
+  getVietMapLightVectorTile = () => {
+    return "https://maps.vietmap.vn/mt/lm/style.json?apikey=95f852d9f8c38e08ceacfd456b59059d0618254a50d3854c";
   };
 }
 export const mapUtils = new MapUtils();
